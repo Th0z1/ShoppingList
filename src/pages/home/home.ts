d77c67ce928bd64f4820cf15ba3c16622f2c9160
Mthokozisi <mthokozisimahlangu0@gmail.com>
2018-08-03 13:27:35 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,65 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams} from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+declare var firebase;
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  
+  
+  name;
+items = [];
 
-  constructor(public navCtrl: NavController) {
+
+
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
   }
+  ionViewDidLoad() {
+       this.UpdateData();
+    }
+    UpdateData(){
+      this.items = [];
+      firebase.database().ref('/Shopper/').on("value", (snapshot) =>{
+        snapshot.forEach((snap) =>{
+          this.items.push({key : snap.key, name:snap.val()});
+
+          return false;
+        })
+      })
+    }
+    add(){
+      this.items = [];
+      console.log(this.name);
+      var database = firebase.database();
+      database.ref('/Shopper/').push(this.name);
+      this.UpdateData();
+      console.log(this.items);
+    }
+    delete(key){
+      var database = firebase.database();
+      database.ref('/Shopper/' +key).remove();
+      this.UpdateData();
+    }
+    update(key){
+      this.name = "Shopped"
+      this.name = '';
+      var database = firebase.database();
+      database.ref('/Shopper/' +key).set(this.name);
+      this.UpdateData();
+    }
 
 }
